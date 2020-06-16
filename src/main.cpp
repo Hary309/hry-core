4e89f7b9f1821916431b6f8fe5c971f400796a7d
Piotr Krupa <piotrkrupa06@gmail.com>
2020-06-16 03:07:41 +0200

    Added scs telemetry init and shutdown

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,21 @@
 
 #include <MinHook.h>
 
+#include <scssdk_telemetry.h>
+
 #include "Core.hpp"
 
 std::unique_ptr<hry::Core> core;
+
+__declspec(dllexport) SCSAPI_RESULT scs_telemetry_init(const scs_u32_t version, const scs_telemetry_init_params_t *const params)
+{
+    return SCS_RESULT_ok;
+}
+
+__declspec(dllexport) SCSAPI_VOID scs_telemetry_shutdown(void)
+{
+    core.reset();
+}
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
