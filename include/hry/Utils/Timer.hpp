3773672d6fabbf97c96814ea929ee9fb9ddaff03
Piotr Krupa <piotrkrupa06@gmail.com>
2020-07-05 02:39:46 +0200

    Added Timer class

---
+++ include/hry/Utils/Timer.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <chrono>
+
+namespace hry::utils
+{
+
+class Timer
+{
+public:
+    using Clock_t = std::chrono::high_resolution_clock;
+    using TimePoint_t = Clock_t::time_point;
+    using Seconds_t = std::chrono::duration<float, std::ratio<1>>;
+    using Milliseconds_t = std::chrono::milliseconds;
+    using Microseconds_t = std::chrono::microseconds;
+
+private:
+    TimePoint_t _start;
+
+public:
+    Timer() : _start(Clock_t::now()) { }
+
+    void reset()
+    {
+        _start = Clock_t::now();
+    }
+
+    auto elapsed() const
+    {
+        return Clock_t::now() - _start;
+    }
+
+    float asSeconds() const 
+    {
+        return std::chrono::duration_cast<Seconds_t>(elapsed()).count();
+    }
+
+    int64_t asMiliseconds() const 
+    {
+        return std::chrono::duration_cast<Milliseconds_t>(elapsed()).count();
+    }
+
+    int64_t asMicrosecond() const 
+    {
+        return std::chrono::duration_cast<Microseconds_t>(elapsed()).count();
+    }
+};
+
+}
