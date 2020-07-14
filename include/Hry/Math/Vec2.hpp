a2cce1162608ea1ba732ebb3c21232ff737fe947
Piotr Krupa <piotrkrupa06@gmail.com>
2020-07-14 17:50:17 +0200

    Added Vec2 struct

---
+++ include/Hry/Math/Vec2.hpp
@@ -0,0 +1,92 @@
+// Original code: https://github.com/SFML/SFML
+// This is a part of SFML under https://github.com/SFML/SFML/blob/master/license.md
+
+#pragma once
+
+namespace hry::math
+{
+
+template<typename T>
+struct Vec2
+{
+	T x;
+	T y;
+};
+
+template <typename T>
+constexpr Vec2<T> operator -(const Vec2<T>& right)
+{
+	return Vec2<T>(-right.x, -right.y);
+}
+
+template <typename T>
+constexpr Vec2<T>& operator +=(Vec2<T>& left, const Vec2<T>& right)
+{
+	left.x += right.x;
+	left.y += right.y;
+
+	return left;
+}
+
+template <typename T>
+constexpr Vec2<T>& operator -=(Vec2<T>& left, const Vec2<T>& right)
+{
+	left.x -= right.x;
+	left.y -= right.y;
+
+	return left;
+}
+
+template <typename T>
+constexpr Vec2<T> operator +(const Vec2<T>& left, const Vec2<T>& right)
+{
+	return Vec2<T>(left.x + right.x, left.y + right.y);
+}
+
+template <typename T>
+constexpr Vec2<T> operator -(const Vec2<T>& left, const Vec2<T>& right)
+{
+	return Vec2<T>(left.x - right.x, left.y - right.y);
+}
+
+template <typename T>
+constexpr Vec2<T> operator *(const Vec2<T>& left, T right)
+{
+	return Vec2<T>(left.x * right, left.y * right);
+}
+
+template <typename T>
+constexpr Vec2<T> operator *(T left, const Vec2<T>& right)
+{
+	return Vec2<T>(left.x * right, left.y * right);
+}
+
+template <typename T>
+constexpr Vec2<T>& operator *=(Vec2<T>& left, T right)
+{
+	left.x *= right;
+	left.y *= right;
+
+	return left;
+}
+
+template <typename T>
+constexpr Vec2<T> operator /(const Vec2<T>& left, T right)
+{
+	return Vec2<T>(left.x / right, left.y / right);
+}
+
+template <typename T>
+constexpr Vec2<T>& operator /=(Vec2<T>& left, T right)
+{
+	left.x /= right;
+	left.y /= right;
+
+	return left;
+}
+
+using Vec2f = Vec2<float>;
+using Vec2i = Vec2<int>;
+using Vec2u = Vec2<unsigned int>;
+
+}
