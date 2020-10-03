877831b9ecb829a34b591756a8ffe333273e3f94
Piotr Krupa <piotrkrupa06@gmail.com>
2020-10-04 01:49:20 +0200

    Remove linking xinput

--- vendor/imgui/imconfig.h
+++ vendor/imgui/imconfig.h
@@ -13,6 +13,8 @@
 
 #pragma once
 
+#define IMGUI_IMPL_WIN32_DISABLE_GAMEPAD
+
 //---- Define assertion handler. Defaults to calling assert().
 // If your macro uses multiple statements, make sure is enclosed in a 'do { .. } while (0)' block so it can be used as a single statement.
 //#define IM_ASSERT(_EXPR)  MyAssert(_EXPR)
