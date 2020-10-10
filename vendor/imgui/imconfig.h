916ed3a726b5d031c688394962d18e52bc50122e
Piotr Krupa <piotrkrupa06@gmail.com>
2020-10-10 18:48:53 +0200

    Update Dear ImGui to 1.79

--- vendor/imgui/imconfig.h
+++ vendor/imgui/imconfig.h
@@ -14,6 +14,7 @@
 #pragma once
 
 #define IMGUI_IMPL_WIN32_DISABLE_GAMEPAD
+#define IMGUI_API __declspec(dllexport)
 
 //---- Define assertion handler. Defaults to calling assert().
 // If your macro uses multiple statements, make sure is enclosed in a 'do { .. } while (0)' block so it can be used as a single statement.
